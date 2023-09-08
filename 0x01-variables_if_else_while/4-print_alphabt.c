6711a165f557d58bd1adb854485046702f32878d
Emanesk19 <emaneskinder57@gmail.com>
2023-09-08 10:44:35 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char c1;
+
+	c1 = 'a';
+
+	while (c1 <= 'z')
+	{
+		putchar(c1);
+		putchar('\n');
+		c1++;
+	}
+	return (0);
+}
