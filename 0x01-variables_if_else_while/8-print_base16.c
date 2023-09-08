9133c0bde2ff7a3a6faa4ce01c6d4980344805e7
Emanesk19 <emaneskinder57@gmail.com>
2023-09-08 11:53:16 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - main function 
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+	char j;
+
+	i = 0;
+	j = 'a';
+
+	while (i < 10)
+	{
+		putchar('0' + (i % 10);
+		i++;
+	}
+	while (j <= 'f')
+	{
+		putchar(j);
+		j++;
+	}
