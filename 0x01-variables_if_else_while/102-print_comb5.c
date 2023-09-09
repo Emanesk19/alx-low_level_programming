214226e0752a8aee30efcedc70fefe4735fbc30d
Emanesk19 <emaneskinder57@gmail.com>
2023-09-09 11:47:27 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,11 +16,14 @@ int main(void)
 		j = i;
 		while (j < 100)
 		{
+			if ((i / 10) != 0 && (i % 10) != 0 && (j / 10) != 0 && (j % 10) != 0)
+			{	
 			putchar(i / 10 + '0');
 			putchar(i % 10 + '0');
 			putchar(' ');
 			putchar(j / 10 + '0');
 			putchar(j % 10 + '0');
+			}
 			if (i < 98 || j < 99)
 			{
 				putchar(',');
