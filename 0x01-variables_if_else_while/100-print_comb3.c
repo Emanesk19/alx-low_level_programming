fefbfb4eca42d2825556f83661b337b45cc0dff9
Emanesk19 <emaneskinder57@gmail.com>
2023-09-09 01:32:23 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,25 +7,25 @@
  */
 int main(void)
 {
-    int i,j;
+	int i;
 
-    i = 0;
-    while (i < 10)
-    {
-        j = i + 1;
-        while (j < 10)
-        {
-            putchar(i + '0');
-            putchar(j + '0');
-            if (i < 8 || j < 9)
-            {
-                putchar(',');
-                putchar(' ');
-            }
-            j++;
-        }
-        i++;
-    }
-    putchar('\n');
-    return (0);
+	i = 0;
+	while (i < 10)
+	{
+		j = i + 1;
+		while (j < 10)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if (i < 8 || j < 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
 }
