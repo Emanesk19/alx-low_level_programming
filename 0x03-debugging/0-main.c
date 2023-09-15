f5f65b8f26e9cb007505d10c8f46299983f56301
Emanesk19 <emaneskinder57@gmail.com>
2023-09-15 06:41:40 -0700

    1-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - test function for posetive and negative
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	
+	postitive_or_negative(i);
+	return (0);
+}
