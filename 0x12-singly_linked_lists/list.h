3a408560349ceb4a78ee4e62b55b2c658431272b
Emanesk19 <emaneskinder57@gmail.com>
2023-10-19 11:05:24 -0700

    0

---
+++ 0x12-singly_linked_lists/list.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
+#endif
